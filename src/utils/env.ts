38095dfd28eb3d8485a16f307daf530fbbf148e8
nqchien1 <nqchien1@cmcglobal.vn>
2022-11-07 11:08:11 +0700

    new(integrate-license-aggrid):DLG-682 add license to web app

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,4 +4,5 @@ export const APP_CONFIG = {
   imgUrl: process.env.IMAGE_URL,
   profileKey: process.env.USER_PROFILE_KEY,
   tokenKey: process.env.TOKEN_KEY,
+  aggrid_license: process.env.AG_GRID_LICENSE_KEY,
 };
